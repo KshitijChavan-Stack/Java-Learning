26265e4aeb623c93ba7b56816885ecd619b496f0
Kshitij_chavan <144587821+ikshitijhere@users.noreply.github.com>
2025-05-31 11:54:36 +0530

    Added Conditionals.java - practicing for loop and while loop

---
+++ Conditionals.java
@@ -0,0 +1,80 @@
+public class Conditionals {
+    //comaprison operators
+    //logical operaors
+    // contional statements
+    // Loops
+
+    public static void main(String[] args) {
+        //COMPARISON OPERATORS
+
+        int x = 1;
+        int y = 1;
+        System.out.println(x==y); // to check equality (==,<,>,=<,=>) boolen expression
+
+        //LOGICAL OPERATOR
+        int TEMP = 22;
+        boolean is_Warm = TEMP>20 && TEMP<30; //(&&) is logical operator both should be true
+        System.out.println(is_Warm);
+
+
+        boolean has_HighIncome = true;
+        boolean has_GoodCredit = false;
+        boolean has_CrimnalRecord = false;
+        boolean is_Eligible = (has_GoodCredit || has_HighIncome) && !has_CrimnalRecord ; // anyone of this need to true
+                                                                    // ! operator makes false to true
+        System.out.println(is_Eligible);
+
+        //IF STATEMENTS
+
+        int TEMPerature =  18;
+        if (TEMPerature > 30){
+            System.out.println("Its a hot day,Drink plenty of water");
+        } else if (TEMPerature <= 30 && TEMPerature > 20) {
+            System.out.println("Its a Nice Day..");
+        }else {
+            System.out.println("It's Cold");
+        }
+
+        //SIMPLYFING IF STATEMENT
+
+
+        int income = 120_000;
+        boolean hasHighIncome = (income>100_000);
+
+        //TERNARAY OPERATOR
+
+//        int income1 = 120_000;
+//        String ClassName= "Economy";
+//        if(income1>100_000)
+//            ClassName = "First";
+
+        int income1 = 120_000;
+        String ClassName= income1 > 100_000 ? "First" : "Economy";
+
+        System.out.println(ClassName);
+
+        //SWITCH STATEMENTS
+
+
+        String role = "Admin";
+        if (role == "Admin")
+            System.out.println("Your an Admin");
+        else if(role == "moderator")
+            System.out.println("your a moderator");
+        else
+            System.out.println("Your Guest");
+
+        switch (role){
+            case "Admin":
+                System.out.println("your Admin");
+                break;
+
+            case "moderator":
+                System.out.println("Your Moderator");
+                break;
+
+            default:
+                System.out.println("your guest");
+        }
+    }
+}
